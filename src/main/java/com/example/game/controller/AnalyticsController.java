1878d25284bb24c16447656fd3495902d9bdc928
JuliaCherepnev <yuliya.cherepnev@mail.ru>
2025-02-21 18:03:38 +0400

    add ResponseEntity<>

--- src/main/java/com/example/game/controller/AnalyticsController.java
+++ src/main/java/com/example/game/controller/AnalyticsController.java
@@ -6,6 +6,7 @@ import com.example.game.service.AnalyticsService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,22 +26,25 @@ public class AnalyticsController {
 
     // Эндпоинт для получения users пользователей с наибольшим значением "money" по каждой стране
     @GetMapping("/top-users-by-money")
-    public List<UserData> getTopUsersByMoneyPerCountry(@RequestParam String country, @RequestParam int usersCount) {
+    public ResponseEntity<List<UserData>> getTopUsersByMoneyPerCountry(@RequestParam String country, @RequestParam int usersCount) {
         log.info("Fetching top {} users by money for country: {}", usersCount, country);
-        return analyticsService.getTopUsersByMoneyPerCountry(country, usersCount);
+        List<UserData> topUsers = analyticsService.getTopUsersByMoneyPerCountry(country, usersCount);
+        return ResponseEntity.ok(topUsers);
     }
 
     // Эндпоинт для подсчета новых пользователей по каждой стране за период времени
     @GetMapping("/new-users-count")
-    public long countNewUsersByCountry(@RequestParam String country, @RequestParam LocalDate startDate) {
+    public ResponseEntity<Long> countNewUsersByCountry(@RequestParam String country, @RequestParam LocalDate startDate) {
         log.info("Counting new users for country: {} from date: {}", country, startDate);
-        return analyticsService.countNewUsersByCountry(country, startDate);
+        long count = analyticsService.countNewUsersByCountry(country, startDate);
+        return ResponseEntity.ok(count);
     }
 
     // Эндпоинт для получения отсортированного списка активности пользователя за период времени
     @GetMapping("/user-activity-history")
-    public List<UserActivityHistory> getUserActivityHistory(@RequestParam String uuid, @RequestParam LocalDate startDate) {
+    public ResponseEntity<List<UserActivityHistory>> getUserActivityHistory(@RequestParam String uuid, @RequestParam LocalDate startDate) {
         log.info("Fetching activity history for user: {} from date: {}", uuid, startDate);
-        return analyticsService.getUserActivityHistory(uuid, startDate);
+       List<UserActivityHistory> activityHistory = analyticsService.getUserActivityHistory(uuid, startDate);
+        return ResponseEntity.ok(activityHistory);
     }
 }
--- src/main/java/com/example/game/controller/UserDataController.java
+++ src/main/java/com/example/game/controller/UserDataController.java
@@ -4,6 +4,7 @@ import com.example.game.service.UserDataService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,23 +16,25 @@ public class UserDataController {
 
     // Эндпоинт для приема данных синхронизации
     @PostMapping("/sync")
-    public String syncUserData(@RequestParam String uuid, @RequestBody String jsonData) {
+    public ResponseEntity<String> syncUserData(@RequestParam String uuid, @RequestBody String jsonData) {
         log.info("Syncing user data for uuid: {}", uuid);
-        return userDataService.processSyncData(uuid, jsonData);
+        String syncData = userDataService.processSyncData(uuid, jsonData);
+        return ResponseEntity.ok(syncData);
     }
 
     // Эндпоинт для отправки данных пользователю
     @GetMapping("/user-data")
-    public String getUserData(@RequestParam String uuid) {
+    public ResponseEntity<String> getUserData(@RequestParam String uuid) {
         log.info("Fetching user data for uuid: {}", uuid);
-        return userDataService.getUserData(uuid);
+        String userData = userDataService.getUserData(uuid);
+        return ResponseEntity.ok(userData);
     }
 
     // Эндпоинт для приема игровой статистики
     @PostMapping("/activity")
-    public String processActivity(@RequestParam String uuid, @RequestParam int activity) {
+    public ResponseEntity<String> processActivity(@RequestParam String uuid, @RequestParam int activity) {
         log.info("Processing activity for uuid: {}, activity: {}", uuid, activity);
-        return userDataService.processActivityData(uuid, activity);
+        String activityData = userDataService.processActivityData(uuid, activity);
+        return ResponseEntity.ok(activityData);
     }
-
 }
